e7959c3719eeea1a45c5dca3d45bc2e8bd44fa48
Fermin <ferkrodriguez98@gmail.com>
2017-08-31 14:33:56 -0300

    Auto stash before merge of "master" and "origin/master"

--- database/seeds/SkillsSeeder.php
+++ database/seeds/SkillsSeeder.php
@@ -13,17 +13,17 @@ class SkillsSeeder extends Seeder
     public function run()
     {
       $skills = [
-        'C++'   => '#076390',
-        'JAVA'  => '#f99924',
-        'PY'    => '#1e415e',
-        'JS'    => '#f0da50',
-        'PHP'   => '#5f83bb',
-        'C#'    => '#662e93',
-        'SWIFT' => '#f57939',
-        'RUBY'  => '#e14e40',
-        'HTML'  => '#f16529',
-        'CSS'   => '#33a9dc',
-        'SQL'   => '#e0e1e3'
+        'C++'         => '#076390',
+        'Java'        => '#f99924',
+        'Python'      => '#1e415e',
+        'JavaScript'  => '#f0da50',
+        'PHP'         => '#5f83bb',
+        'C#'          => '#662e93',
+        'Swift'       => '#f57939',
+        'Ruby'        => '#e14e40',
+        'HTML'        => '#f16529',
+        'CSS'         => '#33a9dc',
+        'SQL'         => '#e0e1e3'
       ];
 
       foreach ($skills as $name => $color) {
